7a3b65e38a261ec8d9807eb6dfc25c6dfc485ff1
aereal <aereal@aereal.org>
2013-12-15 01:39:58 +0900

    Take coverage with coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
 if ENV['COVERAGE'] == '1'
   require 'simplecov'
+  require 'coveralls'
   SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
     SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter,
   ]
   SimpleCov.start do
     add_filter '/spec/'
